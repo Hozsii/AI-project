5fec619f34dad29c30e3c1c79fa49e3309ebd6c0
Hozsii <131807273+Hozsii@users.noreply.github.com>
2025-02-11 20:55:49 +0000

    Added basic form handling and added Typewriter plugin

--- src/script.js
+++ src/script.js
@@ -1,1 +1,13 @@
+function generatePoem(event) {
+  event.preventDefault();
 
+  new Typewriter("#poem", {
+    strings: "A black cat among roses",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
