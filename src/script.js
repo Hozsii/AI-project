849f11b7634a798b21deb932296758dce436bd80
Hozsii <131807273+Hozsii@users.noreply.github.com>
2025-02-13 15:33:57 +0000

    Improved the HTML and CSS

--- src/script.js
+++ src/script.js
@@ -20,7 +20,7 @@ function generatePoem(event) {
 
   let poemElement = document.querySelector("#poem");
   poemElement.classList.remove("hidden");
-  poemElement.innerHTML = `<div class "generating"> ⏳ Generating an English poem about ${instructionsInput.value}</div>`;
+  poemElement.innerHTML = `<div class "generating"> ⏳ Generating a poem about ${instructionsInput.value} ...</div>`;
 
   axios.get(apiUrl).then(displayPoem);
 }
